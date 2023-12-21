5aabd33731ba595636393ee0b0d8ffc4b1b0731e
jawed7siddiqui@gmail.com <jawed7siddiqui@gmail.com>
2023-12-22 02:55:45 +0530

    test3

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ server.get("/api/home", async (req, res) => {
     status: 200,
     success: true,
     data: null,
-    message: "Welcome to the home API! updated and CI tested-231",
+    message: "Welcome to the home API! updated and CI tested-hello",
   });
 });
 
