b5ef9ce7ea51e5106ea1d09c1f4f7bb51d035442
suvam778781 <suvampandar@gmail.com>
2024-02-10 22:59:40 +0530

    fix: fancy result and change condition of match ids

--- routes/matchresult.route.js
+++ routes/matchresult.route.js
@@ -3,9 +3,10 @@ const {
   ResultDeclaireForBookmaker,
   ResultDeclaireTossAndFancyBets,
 } = require("../controllers/matchresult.controller");
+const { ProcessTossAndFancyBets } = require("../controllers/resultcontroller/tossandfancy.controller");
 const ResultRoute = express.Router();
 
 ResultRoute.patch("/update-result/:match_id", ResultDeclaireForBookmaker);
-ResultRoute.patch("/update-fancy-toss-result", ResultDeclaireTossAndFancyBets);
+ResultRoute.patch("/update-fancy-toss-result", ProcessTossAndFancyBets);
 
 module.exports = { ResultRoute };
